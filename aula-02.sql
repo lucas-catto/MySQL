d7c5b499cbbca142c7f9971e8a4adb12326fd388
Lucas Silva <lucascattosilva2018@gmail.com>
2023-07-08 20:07:03 -0300

    Aula 2a - Instalando o MySQL com WAMP

---
+++ aula-02.sql
@@ -0,0 +1,3 @@
+
+-- Aula 2a – Instalando o MySQL com WAMP
+-- Veja como Instalar o WAMP Server no seu computador local e usar o MySQL resolvendo o possível erro “missing file MSVCR100.DLL” instalando as bibliotecas da Microsoft.
